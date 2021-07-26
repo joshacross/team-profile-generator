93a5c0e1233f10786967df69dab5be46fa72845a
Killa J <joshuaalexandercross@gmail.com>
2021-07-25 20:51:34 -0500

    debugged lib/index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const Team = require('./lib/index');
 
-new Team.initializeTeam()
-    .then(teamBuilderData => {
-    return generatePage(teamBuilderData);
-    })
-    .then(templateData => {
-    return writeFile(templateData);
-    })
-    .catch(err => {
-    console.log(err);
-    });
+new Team().initializeTeam();
+    // .then(teamBuilderData => {
+    // return generatePage(teamBuilderData);
+    // })
+    // .then(templateData => {
+    // return writeFile(templateData);
+    // })
+    // .catch(err => {
+    // console.log(err);
+    // });
